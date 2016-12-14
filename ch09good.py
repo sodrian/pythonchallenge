108dee345acd62c747842617b610dc92f4b69bde
Dmitriy Berkutov <sodrian@gmail.com>
2016-12-14 16:51:18 +0300

    polishing

---
+++ ch09good.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import re
+import requests
+from PIL import Image, ImageDraw
+from requests.auth import HTTPBasicAuth
+
+
+URL = 'http://www.pythonchallenge.com/pc/return/good.html'
+FIRST_INPUT_PATTERN = r'first:\s+(.+)\s+second'
+SECOND_INPUT_PATTERN = r'second:\s+(.+)\s+-->'
+
+
+def _raw_to_lst(raw):
+    raw = raw.replace('\n', '')
+    raw = raw.split(',')
+    raw = [int(el) for el in raw]
+    return raw
+
+
+if __name__ == '__main__':
+    body = requests.get(URL, auth=HTTPBasicAuth('huge', 'file')).content
+    first = _raw_to_lst(re.findall(FIRST_INPUT_PATTERN, body, re.DOTALL)[0])
+    second = _raw_to_lst(re.findall(SECOND_INPUT_PATTERN, body, re.DOTALL)[0])
+
+    image = Image.new('RGB', (640,480))
+    draw = ImageDraw.Draw(image)
+    draw.line(first)
+    draw.line(second)
+    image.show()
